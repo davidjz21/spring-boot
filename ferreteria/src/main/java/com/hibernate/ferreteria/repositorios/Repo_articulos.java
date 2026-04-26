4b75a67e5196aaef1d29891b141532d18c66bda4
davidjz21 <186230553+davidjz21@users.noreply.github.com>
2026-04-26 16:59:23 -0500

    saque el proyecto ferreteria como un proyecto aparte

--- ferreteria/src/main/java/com/hibernate/ferreteria/repositorios/Repo_articulos.java
+++
@@ -1,8 +0,0 @@
-package com.hibernate.ferreteria.repositorios;
-
-import com.hibernate.ferreteria.entity.Articulos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repo_articulos extends JpaRepository<Articulos, Long> {
-
-}
